45eb14c01d820f8edda24bf5eff4a3a3890eed8a
stmarcelo <stmarcelo@yahoo.com.br>
2022-08-21 14:24:38 -0300

    Initial commit

---
+++ src/main/java/com/estudo/parking/repository/ParkingRepository.java
@@ -0,0 +1,10 @@
+package com.estudo.parking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.estudo.parking.model.Parking;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<Parking, String> {
+}
---
+++ src/test/java/one/digitalinnovation/parking/service/ParkingCheckOutTest.java
@@ -0,0 +1,11 @@
+package com.estudo.parking.service;
+
+import org.junit.jupiter.api.Test;
+
+class ParkingCheckOutTest {
+
+    @Test
+    void getBill() {
+
+    }
+}
